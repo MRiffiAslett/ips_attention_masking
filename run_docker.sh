80d6c83f1399f2095135d31ffcc1b4b004b82bc6
Max Thibault Riffi-Aslett <121669562+MRiffiAslett@users.noreply.github.com>
2024-07-03 12:43:51 +0100

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Start rootless Docker daemon
+module load rootless-docker
+start_rootless_docker.sh --quiet
+
+# Set Docker host environment variable
+export DOCKER_HOST=unix:///var/tmp/xdg_runtime_dir_$UID/docker.sock
+
 # Variables
 IMAGE_NAME="my-custom-image"
 REPO_URL="https://github.com/MRiffiAslett/ips_attention_masking.git"
-REPO_DIR="ips_attention_masking"
-MAIN_SCRIPT_PATH="main.py"
-DATA_SCRIPT_PATH="data/megapixel_mnist/make_mnist.py"
-DATA_DIR="data/megapixel_mnist/dsets/megapixel_mnist_1500"
-OUTPUT_FILE="results/results_attention_masking_01_28_28.txt"
+REPO_DIR="/app/ips_attention_masking"
+MAIN_SCRIPT_PATH="/app/ips_attention_masking/main.py"
+DATA_SCRIPT_PATH="/app/ips_attention_masking/data/megapixel_mnist/make_mnist.py"
+DATA_DIR="/app/ips_attention_masking/data/megapixel_mnist/dsets/megapixel_mnist_1500"
+OUTPUT_FILE="/app/results_regularized_28_28.txt"
 
 # 1. Build the Docker image
 docker build -t $IMAGE_NAME .
@@ -15,8 +22,8 @@ docker build -t $IMAGE_NAME .
 # 2. Run the Docker container and mount the repository
 docker run --gpus all --runtime=nvidia -it --rm -v "$(pwd)":/app $IMAGE_NAME bash -c "
   # 3. Clone the repository inside the container
-  if [ ! -d /app/$REPO_DIR ]; then
-    git clone $REPO_URL /app/$REPO_DIR
+  if [ ! -d $REPO_DIR ]; then
+    git clone $REPO_URL $REPO_DIR
   fi
 
   # 4. Run the data script to ensure data is downloaded
