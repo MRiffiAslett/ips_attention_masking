2fc9866c7745def1fa9cdbf237152c780a8d0d29
Max Thibault Riffi-Aslett <121669562+MRiffiAslett@users.noreply.github.com>
2024-07-03 20:12:50 +0100

    Update iterative.py

--- training/iterative.py
+++ training/iterative.py
@@ -1,231 +1,309 @@
 import sys
-import numpy as np
+import math
+
 import torch
+import torch.nn as nn
+from torchvision.models import resnet18, resnet50, ResNet18_Weights, ResNet50_Weights
 
-from utils.utils import adjust_learning_rate
+from utils.utils import shuffle_batch, shuffle_instance
+from architecture.transformer import Transformer, pos_enc_1d
 
-def init_batch(device, conf):
+class IPSNet(nn.Module):
     """
-    Initialize the memory buffer for the batch consisting of M patches
-    """
-    if conf.is_image:
-        mem_patch = torch.zeros((conf.B, conf.M, conf.n_chan_in, *conf.patch_size)).to(device)
-    else:
-        mem_patch = torch.zeros((conf.B, conf.M, conf.n_chan_in)).to(device)
-
-    if conf.use_pos:
-        mem_pos_enc = torch.zeros((conf.B, conf.M, conf.D)).to(device)
-    else:
-        mem_pos_enc = None
-
-    # Init the labels for the batch (for multiple tasks in mnist)
-    labels = {}
-    for task in conf.tasks.values():
-        if task['metric'] == 'multilabel_accuracy':
-            labels[task['name']] = torch.zeros((conf.B, conf.n_class), dtype=torch.float32).to(device)
-        else:
-            labels[task['name']] = torch.zeros((conf.B,), dtype=torch.int64).to(device)
-    
-    return mem_patch, mem_pos_enc, labels
-
-def fill_batch(mem_patch, mem_pos_enc, labels, data, n_prep, n_prep_batch,
-            mem_patch_iter, mem_pos_enc_iter, conf):
-    """
-    Fill the patch, pos enc and label buffers and update helper variables
+    Net that runs all the main components:
+    patch encoder, IPS, patch aggregator and classification head
     """
 
-    n_seq, len_seq = mem_patch_iter.shape[:2]
-    mem_patch[n_prep:n_prep+n_seq, :len_seq] = mem_patch_iter
-    if conf.use_pos:
-        mem_pos_enc[n_prep:n_prep+n_seq, :len_seq] = mem_pos_enc_iter
-    
-    for task in conf.tasks.values():
-        labels[task['name']][n_prep:n_prep+n_seq] = data[task['name']]
-    
-    n_prep += n_seq
-    n_prep_batch += 1
+    def get_conv_patch_enc(self, enc_type, pretrained, n_chan_in, n_res_blocks):
+        # Get architecture for patch encoder
+        if enc_type == 'resnet18': 
+            res_net_fn = resnet18
+            weights=ResNet18_Weights.IMAGENET1K_V1 if pretrained else None
+        elif enc_type == 'resnet50':
+            res_net_fn = resnet50
+            # Resnet50 pretrained weights not used in experiments
+            weights=ResNet50_Weights.IMAGENET1K_V1 if pretrained else None        
 
-    batch_data = (mem_patch, mem_pos_enc, labels, n_prep, n_prep_batch)
+        res_net = res_net_fn(weights=weights)
 
-    return batch_data
-
-def shrink_batch(mem_patch, mem_pos_enc, labels, n_prep, conf):
-    """
-    Adjust batch by removing empty instances (may occur in last batch of an epoch)
-    """
-    mem_patch = mem_patch[:n_prep]
-    if conf.use_pos:
-        mem_pos_enc = mem_pos_enc[:n_prep]
-    
-    for task in conf.tasks.values():
-        labels[task['name']] = labels[task['name']][:n_prep]
-    
-    return mem_patch, mem_pos_enc, labels
-
-def compute_loss(net, mem_patch, mem_pos_enc, criterions, labels, conf):
-    """
-    Obtain predictions, compute losses for each task and get some logging stats
-    """
-
-    # Obtain predictions
-    preds = net(mem_patch, mem_pos_enc)
-
-    # Compute losses for each task and sum them up
-    loss = 0
-    task_losses, task_preds, task_labels = {}, {}, {}
-    for task in conf.tasks.values():
-        t_name, t_act = task['name'], task['act_fn']
-
-        criterion = criterions[t_name]
-        label = labels[t_name]
-        pred = preds[t_name].squeeze(-1)
-
-        if t_act == 'softmax':
-            pred_loss = torch.log(pred + conf.eps)
-            label_loss = label
-        else:
-            pred_loss = pred.view(-1)
-            label_loss = label.view(-1).type(torch.float32)
-
-        task_loss = criterion(pred_loss, label_loss)
-        # for logs
-        task_losses[t_name] = task_loss.item()
-        task_preds[t_name] = pred.detach().cpu().numpy()
-        task_labels[t_name] = label.detach().cpu().numpy()
-
-        loss += task_loss
-    # Average task losses        
-    loss /= len(conf.tasks.values())
-
-    return loss, [task_losses, task_preds, task_labels]
-
-
-def train_one_epoch(net, criterions, data_loader, optimizer, device, epoch, log_writer, conf):
-    """
-    Trains the given network for one epoch according to given criterions (loss functions)
-    """
-
-    # Set the network to training mode
-    net.train()
-
-    # Initialize helper variables
-    n_prep, n_prep_batch = 0, 0 # num of prepared images/batches
-    mem_pos_enc = None
-    start_new_batch = True
-
-    times = [] # only used when tracking efficiency stats
-    # Loop through dataloader
-    for data_it, data in enumerate(data_loader, start=epoch * len(data_loader)):
-        # Move input batch onto GPU if eager execution is enabled (default), else leave it on CPU
-        # Data is a dict with keys `input` (patches) and `{task_name}` (labels for given task)
-        image_patches = data['input'].to(device) if conf.eager else data['input']
-
-        # If starting a new batch, create placeholders for data which are filled later
-        if start_new_batch:
-            mem_patch, mem_pos_enc, labels = init_batch(device, conf)
-            start_new_batch = False
-
-            # If tracking efficiency, record time from here.
-            if conf.track_efficiency:
-                start_event = torch.cuda.Event(enable_timing=True)
-                end_event = torch.cuda.Event(enable_timing=True)
-                start_event.record()
+        if n_chan_in == 1:
+            # Standard resnet uses 3 input channels
+            res_net.conv1 = nn.Conv2d(n_chan_in, 64, kernel_size=7, stride=2, padding=3, bias=False)
         
-        # Apply IPS to input patches
-        mem_patch_iter, mem_pos_enc_iter = net.ips(image_patches)
+        # Compose patch encoder
+        layer_ls = []
+        layer_ls.extend([
+            res_net.conv1,
+            res_net.bn1,
+            res_net.relu,
+            res_net.maxpool,
+            res_net.layer1,
+            res_net.layer2
+        ])
+
+        if n_res_blocks == 4:
+            layer_ls.extend([
+                res_net.layer3,
+                res_net.layer4
+            ])
         
-        # Fill batch placeholders with patches from IPS step
-        batch_data = fill_batch(mem_patch, mem_pos_enc, labels, data, n_prep, n_prep_batch,
-                                mem_patch_iter, mem_pos_enc_iter, conf)
-        mem_patch, mem_pos_enc, labels, n_prep, n_prep_batch = batch_data
+        layer_ls.append(res_net.avgpool)
 
-        # Check if the current batch is full or if it is the last batch
-        batch_full = (n_prep == conf.B)
-        is_last_batch = n_prep_batch == len(data_loader)
+        return nn.Sequential(*layer_ls)
 
-        # Do training step as soon as batch is full (or last batch)
-        if batch_full or is_last_batch:
+    def get_projector(self, n_chan_in, D):
+        return nn.Sequential(
+            nn.LayerNorm(n_chan_in, eps=1e-05, elementwise_affine=False),
+            nn.Linear(n_chan_in, D),
+            nn.BatchNorm1d(D),
+            nn.ReLU()
+        )   
 
-            if not batch_full:
-                # Last batch may not be full, so remove empty instances
-                mem_patch, mem_pos_enc, labels = shrink_batch(mem_patch, mem_pos_enc, labels, n_prep, conf)
+    def get_output_layers(self, tasks):
+        """
+        Create an output layer for each task according to task definition
+        """
+
+        D = self.D
+        n_class = self.n_class
+
+        output_layers = nn.ModuleDict()
+        for task in tasks.values():
+            if task['act_fn'] == 'softmax':
+                act_fn = nn.Softmax(dim=-1)
+            elif task['act_fn'] == 'sigmoid':
+                act_fn = nn.Sigmoid()
             
-            # Calculate and set new learning rate
-            adjust_learning_rate(conf.n_epoch_warmup, conf.n_epoch, conf.lr, optimizer, data_loader, data_it+1)
-            optimizer.zero_grad()
+            layers = [
+                nn.Linear(D, n_class),
+                act_fn
+            ]
+            output_layers[task['name']] = nn.Sequential(*layers)
 
-            # Compute loss
-            loss, task_info = compute_loss(net, mem_patch, mem_pos_enc, criterions, labels, conf)
-            task_losses, task_preds, task_labels = task_info
+        return output_layers
 
-            # Backpropagate error and update parameters
-            loss.backward()
-            optimizer.step()
+    def __init__(self, device, conf):
+        super().__init__()
 
-            # If tracking efficiency, log the time and memory usage
-            if conf.track_efficiency:
-                end_event.record()
-                torch.cuda.synchronize()
-                if epoch == conf.track_epoch and data_it > 0 and not is_last_batch:
-                    times.append(start_event.elapsed_time(end_event))
-                    print("time: ", times[-1])
+        self.device = device
+        self.n_class = conf.n_class
+        self.M = conf.M
+        self.I = conf.I
+        self.D = conf.D 
+        self.use_pos = conf.use_pos
+        self.tasks = conf.tasks
+        self.shuffle = conf.shuffle
+        self.shuffle_style = conf.shuffle_style
+        self.is_image = conf.is_image
+        self.mask_p = conf.mask_p  # Probability of masking
+        self.mask_K = conf.mask_K  # Number of top-K instances to consider for masking
 
-            # Update log
-            log_writer.update(task_losses, task_preds, task_labels)
+        if self.is_image:
+            self.encoder = self.get_conv_patch_enc(conf.enc_type, conf.pretrained,
+                conf.n_chan_in, conf.n_res_blocks)
+        else:
+            self.encoder = self.get_projector(conf.n_chan_in, self.D)
 
-            # Reset helper variables
-            n_prep = 0
-            start_new_batch = True
-    
-    if conf.track_efficiency:
-        if epoch == conf.track_epoch:
-            print("avg. time: ", np.mean(times))
+        # Define the multi-head cross-attention transformer
+        self.transf = Transformer(conf.n_token, conf.H, conf.D, conf.D_k, conf.D_v,
+            conf.D_inner, conf.attn_dropout, conf.dropout)
 
-            stats = torch.cuda.memory_stats()
-            peak_bytes_requirement = stats["allocated_bytes.all.peak"]
-            print(f"Peak memory requirement: {peak_bytes_requirement / 1024 ** 3:.4f} GB")
-
-            print("TORCH.CUDA.MEMORY_SUMMARY: ", torch.cuda.memory_summary())
-            sys.exit()
-
-
-# Disable gradient calculation during evaluation
-@torch.no_grad()
-def evaluate(net, criterions, data_loader, device, log_writer, conf):
-
-    # Set the network to evaluation mode
-    net.eval()
-
-    # Remaining parts similar to training loop
-    n_prep, n_prep_batch = 0, 0
-    mem_pos_enc = None
-    start_new_batch = True
-    
-    for data in data_loader:
-        image_patches = data['input'].to(device) if conf.eager else data['input']
-
-        if start_new_batch:
-            mem_patch, mem_pos_enc, labels = init_batch(device, conf)
-            start_new_batch = False
+        # Optionally use standard 1d sinusoidal positional encoding
+        if conf.use_pos:
+            self.pos_enc = pos_enc_1d(conf.D, conf.N).unsqueeze(0).to(device)
+        else:
+            self.pos_enc = None
         
-        mem_patch_iter, mem_pos_enc_iter = net.ips(image_patches)
+        # Define an output layer for each task
+        self.output_layers = self.get_output_layers(conf.tasks)
+
+    def do_shuffle(self, patches, pos_enc):
+        """
+        Shuffles patches and pos_enc so that patches that have an equivalent score
+        are sampled uniformly
+        """
+
+        shuffle_style = self.shuffle_style
+        if shuffle_style == 'batch':
+            patches, shuffle_idx = shuffle_batch(patches)
+            if torch.is_tensor(pos_enc):
+                pos_enc, _ = shuffle_batch(pos_enc, shuffle_idx)
+        elif shuffle_style == 'instance':
+            patches, shuffle_idx = shuffle_instance(patches, 1)
+            if torch.is_tensor(pos_enc):
+                pos_enc, _ = shuffle_instance(pos_enc, 1, shuffle_idx)
         
-        batch_data = fill_batch(mem_patch, mem_pos_enc, labels, data, n_prep, n_prep_batch,
-                                mem_patch_iter, mem_pos_enc_iter, conf)
-        mem_patch, mem_pos_enc, labels, n_prep, n_prep_batch = batch_data
+        return patches, pos_enc
 
-        batch_full = (n_prep == conf.B)
-        is_last_batch = n_prep_batch == len(data_loader)
+    def score_and_select(self, emb, emb_pos, M, idx):
+        """ 
+        Scores embeddings and selects the top-M embeddings with stochastic masking
+        """
+        D = emb.shape[2]
 
-        if batch_full or is_last_batch:
+        emb_to_score = emb_pos if torch.is_tensor(emb_pos) else emb
 
-            if not batch_full:
-                mem_patch, mem_pos_enc, labels = shrink_batch(mem_patch, mem_pos_enc, labels, n_prep, conf)
+        # Obtain scores from the transformer
+        attn = self.transf.get_scores(emb_to_score)  # (B, M+I)
+
+        # 1. Get indices of top-K patches for masking
+        top_K_idx = torch.topk(attn, self.mask_K, dim=-1)[1]  # (B, K)
+
+        # 2. Create a mask with probability p for top-K instances
+        mask = (torch.rand(top_K_idx.shape, device=attn.device) < self.mask_p).float()
+
+        # 3. Apply the mask to the top-K attention scores
+        # Scatter the mask over the top-K indices to set some of them to 0
+        attn.scatter_(1, top_K_idx, attn.gather(1, top_K_idx) * (1 - mask))
+
+        # 4. Get indices of top-M patches after masking
+        top_idx = torch.topk(attn, M, dim=-1)[1]  # (B, M)
+
+        # Update memory buffers
+        # Note: Scoring is based on `emb_to_score`, selection is based on `emb`
+        mem_emb = torch.gather(emb, 1, top_idx.unsqueeze(-1).expand(-1, -1, D))
+        mem_idx = torch.gather(idx, 1, top_idx)
+
+        return mem_emb, mem_idx
+
+    def get_preds(self, embeddings):
+        preds = {}
+        for task in self.tasks.values():
+            t_name, t_id = task['name'], task['id']
+            layer = self.output_layers[t_name]
+
+            emb = embeddings[:,t_id]
+            preds[t_name] = layer(emb)            
+
+        return preds
+
+    # IPS runs in no-gradient mode
+    @torch.no_grad()
+    def ips(self, patches):
+        """ Iterative Patch Selection """
+
+        # Get useful variables
+        M = self.M
+        I = self.I
+        D = self.D  
+        device = self.device
+        shuffle = self.shuffle
+        use_pos = self.use_pos
+        pos_enc = self.pos_enc
+        patch_shape = patches.shape
+        B, N = patch_shape[:2]
+
+        # Shortcut: IPS not required when memory is larger than total number of patches
+        if M >= N:
+            # Batchify pos enc
+            pos_enc = pos_enc.expand(B, -1, -1) if use_pos else None
+            return patches.to(device), pos_enc 
+
+        # IPS runs in evaluation mode
+        if self.training:
+            self.encoder.eval()
+            self.transf.eval()
+
+        # Batchify positional encoding
+        if use_pos:
+            pos_enc = pos_enc.expand(B, -1, -1)
+
+        # Shuffle patches (i.e., randomize when patches obtain identical scores)
+        if shuffle:
+            patches, pos_enc = self.do_shuffle(patches, pos_enc)
+
+        # Init memory buffer
+        # Put patches onto GPU in case it is not there yet (lazy loading).
+        # `to` will return self in case patches are located on GPU already (eager loading)
+        init_patch = patches[:,:M].to(device) 
+        
+        ## Embed
+        mem_emb = self.encoder(init_patch.reshape(-1, *patch_shape[2:]))
+        mem_emb = mem_emb.view(B, M, -1)
+        
+        # Init memory indices in order to select patches at the end of IPS.
+        idx = torch.arange(N, dtype=torch.int64, device=device).unsqueeze(0).expand(B, -1)
+        mem_idx = idx[:,:M]
+
+        # Apply IPS for `n_iter` iterations
+        n_iter = math.ceil((N - M) / I)
+        for i in range(n_iter):
+            # Get next patches
+            start_idx = i * I + M
+            end_idx = min(start_idx + I, N)
+
+            iter_patch = patches[:, start_idx:end_idx].to(device)
+            iter_idx = idx[:, start_idx:end_idx]
+
+            # Embed
+            iter_emb = self.encoder(iter_patch.reshape(-1, *patch_shape[2:]))
+            iter_emb = iter_emb.view(B, -1, D)
             
-            _, task_info = compute_loss(net, mem_patch, mem_pos_enc, criterions, labels, conf)
-            task_losses, task_preds, task_labels = task_info
+            # Concatenate with memory buffer
+            all_emb = torch.cat((mem_emb, iter_emb), dim=1)
+            all_idx = torch.cat((mem_idx, iter_idx), dim=1)
+            # When using positional encoding, also apply it during patch selection
+            if use_pos:
+                all_pos_enc = torch.gather(pos_enc, 1, all_idx.view(B, -1, 1).expand(-1, -1, D))
+                all_emb_pos = all_emb + all_pos_enc
+            else:
+                all_emb_pos = None
 
-            log_writer.update(task_losses, task_preds, task_labels)
+            # Select Top-M patches according to cross-attention scores
+            mem_emb, mem_idx = self.score_and_select(all_emb, all_emb_pos, M, all_idx)
 
-            n_prep = 0
-            start_new_batch = True
+        # Select patches
+        n_dim_expand = len(patch_shape) - 2
+        mem_patch = torch.gather(patches, 1, 
+            mem_idx.view(B, -1, *(1,)*n_dim_expand).expand(-1, -1, *patch_shape[2:]).to(patches.device)
+        ).to(device)
+
+        if use_pos:
+            mem_pos = torch.gather(pos_enc, 1, mem_idx.unsqueeze(-1).expand(-1, -1, D))
+        else:
+            mem_pos = None
+
+        # Set components back to training mode
+        # Although components of `self` that are relevant for IPS have been set to eval mode,
+        # self is still in training mode at training time, i.e., we can use it here.
+        if self.training:
+            self.encoder.train()
+            self.transf.train()
+    
+        # Return selected patch and corresponding positional embeddings
+        return mem_patch, mem_pos
+
+    def forward(self, mem_patch, mem_pos=None):
+        """
+        After M patches have been selected during IPS, encode and aggregate them.
+        The aggregated embedding is input to a classification head.
+        """
+
+        patch_shape = mem_patch.shape
+        B, M = patch_shape[:2]
+
+        mem_emb = self.encoder(mem_patch.reshape(-1, *patch_shape[2:]))
+        mem_emb = mem_emb.view(B, M, -1)        
+
+        if torch.is_tensor(mem_pos):
+            mem_emb = mem_emb + mem_pos
+
+        # Separate main embeddings and image embeddings
+        image_emb = self.transf(mem_emb)[0]
+
+        branch_embeddings = self.transf(mem_emb)[1]
+
+        preds = self.get_preds(image_emb)
+
+        branch_preds = []
+
+        for i in range(branch_embeddings.shape[1]):
+          branch_preds.append(self.get_preds(branch_embeddings[:,i]))
+
+        return preds, branch_preds
+        
+    def compute_diversity_loss(self):
+        """
+        Compute the diversity loss using the attention maps from the transformer.
+        """
+        return self.transf.compute_diversity_loss()
