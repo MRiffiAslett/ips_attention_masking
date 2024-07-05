7c6e7c11589d3f0878c081ae8f37ca6ff2311110
Max Thibault Riffi-Aslett <121669562+MRiffiAslett@users.noreply.github.com>
2024-07-05 19:41:02 +0100

    Update run_batch.sh

--- run_batch.sh
+++ run_batch.sh
@@ -8,10 +8,15 @@ start_rootless_docker.sh --quiet
 IMAGE_NAME="my-custom-image"
 REPO_DIR="$(pwd)"
 RESULTS_DIR="$REPO_DIR/results"
-MAIN_SCRIPT_PATH="/app/ips_attention_masking/main.py"
-DATA_SCRIPT_PATH="/app/ips_attention_masking/data/megapixel_mnist/make_mnist.py"
-DATA_DIR="/app/ips_attention_masking/data/megapixel_mnist/dsets/megapixel_mnist_1500"
-OUTPUT_FILE="/app/results/results_masked_01_28.txt"
+SCRIPT_DIR="/app/ips_attention_masking"
+MAIN_SCRIPT_1_PATH="$SCRIPT_DIR/main_1.py"
+MAIN_SCRIPT_2_PATH="$SCRIPT_DIR/main_2.py"
+MAIN_SCRIPT_3_PATH="$SCRIPT_DIR/main_3.py"
+DATA_SCRIPT_PATH="$SCRIPT_DIR/data/megapixel_mnist/make_mnist.py"
+DATA_DIR="$SCRIPT_DIR/data/megapixel_mnist/dsets/megapixel_mnist_1500"
+OUTPUT_FILE_1="/app/results/results_masked_01_28_main_1.txt"
+OUTPUT_FILE_2="/app/results/results_masked_01_28_main_2.txt"
+OUTPUT_FILE_3="/app/results/results_masked_01_28_main_3.txt"
 
 # Ensure the repository and results directories exist
 if [ ! -d "$REPO_DIR" ]; then
@@ -30,6 +35,8 @@ docker run --gpus all --shm-size=4g -it --rm -v "$REPO_DIR:/app/ips_attention_ma
   # 3. Run the data script to ensure data is downloaded
   python3 $DATA_SCRIPT_PATH --width 1500 --height 1500 $DATA_DIR
 
-  # 4. Run the main script and capture the output
-  unbuffer python3 $MAIN_SCRIPT_PATH | tee $OUTPUT_FILE
+  # 4. Run the main scripts sequentially and capture the output
+  unbuffer python3 $MAIN_SCRIPT_1_PATH | tee $OUTPUT_FILE_1
+  unbuffer python3 $MAIN_SCRIPT_2_PATH | tee $OUTPUT_FILE_2
+  unbuffer python3 $MAIN_SCRIPT_3_PATH | tee $OUTPUT_FILE_3
 "
